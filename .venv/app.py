6400393186f74cca5e8fd401894f6b991c7f7686
webeltek2017@gmail.com <webeltek2017@gmail.com>
2021-12-21 15:43:05 +0100

    commit

--- .venv/app.py
+++ .venv/app.py
@@ -4,5 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     a = "thing"
-    b = "second thing23221654654"
+    b = "second thing2322165465412313"
+    c = "ve3213421341234"
     return "Hello, Flask, and more stuff!"
