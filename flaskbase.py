9db09ac2b1bf647888c3f299f4b8292eafcb0b6c
Webeltek <webeltek2017@gmail.com>
2022-01-13 13:57:11 +0100

    Added simple html and form web server

--- flaskbase.py
+++ flaskbase.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template('html/basic.html')
+
+@app.route("/form")
+def test_form():
+    return render_template('html/form.html')
+
+@app.route("/postit", methods = ['POST'])
+def test_form_post():
+    data = request.form
+    print(data)
+    return 'OK, Igot your data!'
+
 
 if __name__== "__main__":
       app.run()
