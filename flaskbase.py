a790218ffb7d36e3ce139d7fcffca77428a0c599
Webeltek <webeltek2017@gmail.com>
2022-01-13 13:13:41 +0100

    Added basic Flask app

---
+++ flaskbase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template('html/basic.html')
+
+if __name__== "__main__":
+      app.run()
+
+
