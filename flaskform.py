191d3fa1757c64f2752da9ac4979bedf8465947c
Webeltek <webeltek2017@gmail.com>
2022-01-13 15:41:08 +0100

    fixed forms feil

--- flaskform.py
+++ flaskform.py
@@ -5,17 +5,16 @@ from flask import Flask, render_template, request
 from wtforms import Form, StringField, validators
 
 class MyForm(Form):
-    ufoname = StringField('Username', [validators.Length(min=4, max=25)])
+    ufoname = StringField('ufoname', [validators.Length(min=4, max=25)])
 
 app = Flask(__name__,
         static_url_path= '/static',
-        static_folder= '/static',
+        static_folder= 'static',
         template_folder = 'templates')
 
 
 @app.route("/", methods= ['POST','GET'])
 def test_fullform():
-    if request.method == 'GET':
         myform = MyForm(request.form)
         if request.method == 'POST' and myform.validate():
             print(F'I got UFO name is {myform.ufoname.data}')
