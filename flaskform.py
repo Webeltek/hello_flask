14eae10eb0c7f838ea01601496e784a0da1f8742
webeltek2017@gmail.com <webeltek2017@gmail.com>
2022-01-16 00:41:42 +0100

    16 jan corr

--- flaskform.py
+++ flaskform.py
@@ -2,18 +2,23 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, DateTimeField, DecimalField, validators, SubmitField, widgets
 
 class MyForm(Form):
-    ufoname = StringField('ufoname', [validators.Length(min=4, max=25)])
+    ufoname = StringField('ufoname',validators= [validators.Length(min=4, max=25)])
+    name = StringField(label='Name', validators=[validators.Length(min=4, max=25)])
+    timeofbirth = DateTimeField(label='Time of birth',format='%d/%m/%Y %H:%M:%S', widget=widgets.DateTimeInput())
+    height = DecimalField(label='Height',places=2)
+    submitField = SubmitField('Submit')
+
 
 app = Flask(__name__,
-        static_url_path= '/static',
+        static_url_path='/static',
         static_folder= 'static',
         template_folder = 'templates')
 
 
-@app.route("/", methods= ['POST','GET'])
+@app.route("/", methods= ['GET','POST'])
 def test_fullform():
         myform = MyForm(request.form)
         if request.method == 'POST' and myform.validate():
