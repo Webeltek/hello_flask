be218990665203869d5bc16522a10c8ae5316279
Webeltek <webeltek2017@gmail.com>
2022-02-09 00:41:27 +0100

    test15

--- nfform1.py
+++ nfform1.py
@@ -4,15 +4,35 @@
 from flask import Flask, render_template, request, url_for
 from wtforms import Form, StringField, DateTimeField, DecimalField, validators, SubmitField, widgets
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from werkzeug.utils import secure_filename
 import jinja2
 import os
+from flask_wtf.csrf import CSRFProtect
+
 
 class ContactForm(FlaskForm):
-    epost = StringField(label='Epost:', validators=[validators.Length(min=4, max=25),validators.Email()],render_kw={"placeholder":"Firma epost her"})
+    epost = StringField(label='Epost:', validators=[validators.Length(min=4, max=25),validators.Email()])
     telf = StringField(label='Telefon:', validators=[validators.Length(min=8, max=16)])
     twitter = StringField(label='Twitter:', validators=[validators.Length(min=4, max=25)])
     facebook = StringField(label='Facebook:', validators=[validators.Length(min=4, max=25)])
     instagram = StringField(label='Instagram:', validators=[validators.Length(min=4, max=25)])    
+
+class PersonsForm(FlaskForm):
+    epost = StringField(label='Epost:', validators=[validators.Length(min=4, max=25),validators.Email()])
+    telf = StringField(label='Telefon:', validators=[validators.Length(min=8, max=16)])
+    twitter = StringField(label='Twitter:', validators=[validators.Length(min=4, max=25)])
+    facebook = StringField(label='Facebook:', validators=[validators.Length(min=4, max=25)])
+    instagram = StringField(label='Instagram:', validators=[validators.Length(min=4, max=25)])
+
+class ServicesForm(FlaskForm):
+    description = StringField(label='Epost:', validators=[validators.Length(min=4, max=25),validators.InputRequired()])
+    photo = FileField(validators=[FileRequired()])
+
+class AboutUsForm(FlaskForm):
+    firma_navn = StringField(label='Firma navn:', validators=[validators.Length(min=4, max=30)])
+    org_nr = DecimalField(label='OrgNr:', validators=[validators.Length(min=8, max=16)])
+    kort_om = StringField(label='Kort om oss:', validators=[validators.Length(min=4, max=25)])
 
 templ_dir = os.path.abspath('./mockup_v2/templates')
 
@@ -21,6 +41,17 @@ app = Flask(__name__,
      static_folder= 'static',
      template_folder=templ_dir)
 
+csrf = CSRFProtect(app)
+
+templateLoader = jinja2.FileSystemLoader(searchpath='./mockup_v2/templates')
+templateEnv = jinja2.Environment(loader=templateLoader)
+
+user_templ = templateEnv.get_template('user_page.jinja2')
+login_templ = templateEnv.get_template('forms/login.jinja2')
+contact_templ = templateEnv.get_template('forms/contact.jinja2')
+persons_templ = templateEnv.get_template('forms/persons.jinja2')
+services_templ = templateEnv.get_template('forms/services.jinja2')
+about_us_templ = templateEnv.get_template('forms/about_us.jinja2')
 
 @app.route("/", methods= ['GET','POST'])
 def login_form():
@@ -28,40 +59,40 @@ def login_form():
         if request.method == 'POST' :
             print(F'I got UFO name is {myform.ufoname.data}')
             return F'I got your autodata!!!'
-        return render_template('forms/login.jinja2')
+        return login_templ.render()
 
 
 @app.route("/contact", methods= ['GET','POST'])
 def contact_form():
-        myform = ContactForm(request.form)
-        if request.method == 'POST' and myform.validate():
+        contact = ContactForm(request.form)
+        if request.method == 'POST' and contact.validate():
             print(F'I got UFO name is {myform.ufoname.data}')
             return F'I got your autodata!!!'
-        return render_template('forms/contact.jinja2',form=myform)
+        return contact_templ.render(contact=contact)
 
 @app.route("/persons", methods= ['GET','POST'])
 def persons_form():
-        myform = MyForm(request.form)
-        if request.method == 'POST' and myform.validate():
+        persons = PersonsForm(request.form)
+        if request.method == 'POST' and persons.validate():
             print(F'I got UFO name is {myform.ufoname.data}')
             return F'I got your autodata!!!'
         return render_template('forms/persons.jinja2',form=myform)
 
 @app.route("/services", methods= ['GET','POST'])
 def services_form():
-        myform = MyForm(request.form)
-        if request.method == 'POST' and myform.validate():
+        services = ServicesForm(request.form)
+        if request.method == 'POST' and services.validate():
             print(F'I got UFO name is {myform.ufoname.data}')
             return F'I got your autodata!!!'
-        return render_template('forms/services.jinja2',form = myform)    
+        return services_templ.render(services = services)    
 
 @app.route("/about_us", methods= ['GET','POST'])
 def about_us_form():
-        myform = MyForm(request.form)
-        if request.method == 'POST' and myform.validate():
+        about_us = AboutUsForm(request.form)
+        if request.method == 'POST' and about_us.validate():
             print(F'I got UFO name is {myform.ufoname.data}')
             return F'I got your autodata!!!'
-        return render_template('forms/about_us.jinja2',form=myform)
+        return about_us_templ.render(about=about_us)
 
 
 if __name__== "__main__":
